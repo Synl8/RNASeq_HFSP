6f515d2b99908b4ac8d9ec9cbaa523186a89bfe9
Your kaminosekai54 <culpin.alexis@laposte.net>
2022-05-29 18:55:32 +0200

    adding import

--- filter_CL.py
+++ filter_CL.py
@@ -7,6 +7,7 @@ from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from Bio import pairwise2
+import json
 
 
 ################################################################################
