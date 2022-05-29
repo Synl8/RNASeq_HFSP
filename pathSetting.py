492cc56f93c2f3a001e8a7760f7ee4f60bfa2b4e
Your kaminosekai54 <culpin.alexis@laposte.net>
2022-05-29 14:22:23 +0200

    correcting typo

--- pathSetting.py
+++ pathSetting.py
@@ -1,6 +1,6 @@
 databaseFilePath = "pool0_1.fa"
 variantReadFilePath = "L422T14_cl.fa"
-nsuFilePath = "L422T13_cl
+nsuFilePath = "L422T13_cl.fasta"
 
 def getFilePath():
     return databaseFilePath, variantReadFilePath, nsuFilePath, mafftPath 
