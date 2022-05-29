4ae1d07c51cfe1ea0aacbe9983dff069a1b55df6
Synl08 <54885255+Synl08@users.noreply.github.com>
2022-05-29 19:22:08 +0100

    upload file subpool

--- pathSetting.py
+++
@@ -1,6 +0,0 @@
-databaseFilePath = "pool0_1.fa"
-variantReadFilePath = "L422T14_cl.fa"
-nsuFilePath = "L422T13_cl.fa"
-mafftPath = "/usr/bin/mafft"
-def getFilePath():
-    return databaseFilePath, variantReadFilePath, nsuFilePath, mafftPath 
