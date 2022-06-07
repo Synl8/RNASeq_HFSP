73ae5c3aaa19a239b8836720fe55396e1e36b699
Synl08 <54885255+Synl08@users.noreply.github.com>
2022-06-07 13:56:13 +0100

    update file

--- pathSetting_CL.py
+++
@@ -1,7 +0,0 @@
-databaseFilePath = "pool0_1.fa"
-subFilePath = "L422T14_cl.fa"
-nsubFilePath = "L422T13_cl.fa"
-mafftPath = "/usr/bin/mafft"
-
-def getFilePath():
-    return databaseFilePath, subFilePath, nsubFilePath, mafftPath 
