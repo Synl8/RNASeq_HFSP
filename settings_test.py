ac62d27ee1d37637b75fd24410b96f293277b028
Synl08 <54885255+Synl08@users.noreply.github.com>
2022-07-19 10:02:43 +0100

    upload file

---
+++ settings_test.py
@@ -0,0 +1,19 @@
+settings = {
+"databaseFilePath" : "pool0_1.fa",
+"subFilePath" : "L422T11_1_cl.fa",
+"cleanSubFilePath" : "L422T11_1_cl.fa",
+# "cleanSubFilePath" : "L422T14_cl_cleaned.fa",
+"nSubFilePath" : "L422T08_1_cl.fa",
+"cleanNSubFilePath" : "L422T08_1_cl.fa",
+# "cleanNSubFilePath" : "L422T13_cl_cleaned.fa",
+"mafftPath" : "/usr/bin/mafft",
+"exon" : "aatccgttggtgctg",
+"sub2": "AACTTCAAATATCTTCGGAACTCA",
+"3primeCleanerIdentifier" : "GATCGGAAGAGCACA",
+"5primeCleanerIdentifier" : "gtgactggagttcagacgtgtgctcttccgatc",
+"G_test" : "[G]{5,10}$",
+}
+
+
+def getSettings():
+    return settings 
